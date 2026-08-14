6973f346db1b8ff3a3e7d07c8964236ba591dc56
perottofranz94-sudo <perotto.franz94@gmail.com>
2026-08-14 21:03:21 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = "dbw-planner-v4";
+const ASSETS = ["./", "./index.html", "./logo.png", "./icon-192.png", "./icon-512.png", "./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(resp => {
+    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return resp;
+  }).catch(()=>caches.match(event.request).then(r=>r || caches.match("./index.html"))));
+});

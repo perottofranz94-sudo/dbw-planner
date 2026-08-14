72c357504d80dd40dbb10043a6deed6062116c72
perottofranz94-sudo <perotto.franz94@gmail.com>
2026-08-14 21:52:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,5 @@
-const CACHE = "dbw-planner-v5-sync";
-const ASSETS = ["./", "./index.html", "./logo.png", "./icon-192.png", "./icon-512.png", "./manifest.webmanifest"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).then(resp => {
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return resp;
-  }).catch(()=>caches.match(event.request).then(r=>r || caches.match("./index.html"))));
-});
+const CACHE='dbw-planner-v6';
+const ASSETS=['./','./index.html','./logo.png','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
